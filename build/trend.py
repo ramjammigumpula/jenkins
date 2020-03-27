50d8298582f97d9a832d077d1b9275e0516923c6
ramjammigumpula <ramjammigumpula@users.noreply.github.com>
2020-03-27 12:50:37 -0700

    Update trend.py

--- build/trend.py
+++ build/trend.py
@@ -1,17 +1,18 @@
 import sys
 import os
-from pytrends.request import TrendReq
-s=open("file1.csv","w")
-
-curpath = os.path.abspath(os.curdir)
+from pytrends.request import TrendReq #import pytrends to make a http get request 
+# constants
+searchKeyword1 = "blue"
+searchKeyword2 = "red"
+#Create file in write mode to stream response from pytrends to file1
+stream = open("file1.csv", "w")
+currentPath = os.path.abspath(os.curdir)
 pytrend = TrendReq()
-pytrend.build_payload(kw_list=["blue", "red"])
-df = pytrend.interest_over_time()
-s.write(df.to_string())
-#file_name = curpath + "file1.csv"
-#df.to_csv("file1.csv", encoding='utf-8', index=False)
-file = open(os.path.join(curpath, "file2.csv"), 'w')
+pytrend.build_payload(kw_list=[searchKeyword1, searchKeyword2])
+dataFrame = pytrend.interest_over_time()
+stream.write(dataFrame.to_string())
+file = open(os.path.join(currentPath, "file2.csv"), 'w')
 with file as f:
     sys.stdout = f
-    print("Sum of blue queries:" + str(df['blue'].sum()))
-    print("Sum of red queries:" + str(df['red'].sum()))
+    print("Sum of blue queries:" + str(dataFrame[searchKeyword1].sum()))
+    print("Sum of red queries:" + str(dataFrame[searchKeyword2].sum()))
